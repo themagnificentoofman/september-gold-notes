237526a7e920b5d40e24f57e7a145c91f7661c00
Zachary <zacharyzheng2009@gmail.com>
2025-09-28 18:12:48 -0500

    Create template.cpp for competitive programming
    Add template for competitive programming with I/O setup

---
+++ template.cpp
@@ -0,0 +1,32 @@
+// Source: https://usaco.guide/general/io
+// #pragma GCC optimize("Ofast")
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using db = long double;
+template<class T> using V = vector<T>;
+template<class T1, class T2> using P = pair<T1, T2>;
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define f first
+#define s second
+using vi = V<int>;
+using pi = P<int,int>;
+inline namespace FileIO {
+  void setIn(string s){freopen(s.c_str(),"r",stdin);}
+	void setOut(string s){freopen(s.c_str(),"w",stdout);}
+	void setIO(string s = ""){
+		cin.tie(0)->sync_with_stdio(0);
+		cin.exceptions(cin.failbit);
+		if (sz(s)) setIn(s+".in"), setOut(s+".out");
+	}
+}
+const db PI = acos((db)-1);
+
+int main() {
+	setIO();
+    
+}
